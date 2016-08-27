122a6b7ba8249f101b3174ef6f4ed30d648563d8
Max Lebedev <maxlebedev@live.com>
2016-08-27 15:29:15 -0400

    much better formatted output. using a dict to track common cards

--- deckdiff.py
+++ deckdiff.py
@@ -1,5 +1,6 @@
 import re
 import sys
+import collections
 #read in 2 deck files, print out their differences
 
 deckpat = re.compile('(\d+)x?-?\s*(\w.*)')
@@ -8,7 +9,7 @@ notcard = re.compile('^\s*$')
 def deck_to_dict(deck_path):
     deck_file = open(deck_path, 'r')
     deck = deck_file.readlines()
-    deck_dict = dict()
+    deck_dict = collections.defaultdict(int)
     for line in deck:
         if notcard.match(line):
             continue
@@ -18,46 +19,45 @@ def deck_to_dict(deck_path):
         else:
             deck_dict[line.lower()] = 1
 
-    ##collections.Counter(['Lightning Bolt','Lightning Bolt','Lightning Bolt'])
+    ##TODO collections.Counter(['Lightning Bolt','Lightning Bolt','Lightning Bolt'])
     # for if there are no nums
 
     return deck_dict
     #TODO detect sideboard
+
+def dict_to_difflist(deck1, deck2):
+    commondict = dict() #Bolt (d1, both, d2)
+
+    for card in (deck1.viewkeys() | deck2.viewkeys()): #these are lists
+        if deck1[card] == 0:
+            commondict[card] = ('','',deck2[card])
+        elif deck2[card] == 0:
+            commondict[card] = (deck1[card],'','')
+        elif deck1[card] > deck2[card]:
+            diff = deck1[card] - deck2[card]
+            common = deck2[card]
+            commondict[card] = (diff,common,'')
+        elif deck2[card] > deck1[card]:
+            diff = deck2[card] - deck1[card]
+            common = deck1[card]
+            commondict[card] = ('',common,diff)
+        else: #completely shared
+            commondict[card] = ('',deck1[card],'')
+    return commondict
         
-#TODO maybe use counter instead of dict
 def main(deck_paths):
     deck1 = deck_to_dict(deck_paths[0])
     deck2 = deck_to_dict(deck_paths[1])
-    difflist1 = []
-    difflist2 = []
-    ###commondict = dict() #Bolt (1, both, 2)
-    for card in deck1.keys():
-        if card in deck2:
-            diff = deck1[card] - deck2[card]
-            if diff>0:
-                difflist1.append((diff, card))
-        else:
-            difflist1.append((deck1[card], card))
 
-    for card in deck2.keys():
-        if card in deck1:
-            diff = deck2[card] - deck1[card]
-            if diff>0:
-                difflist2.append((diff, card))
-        else:
-            difflist2.append((deck2[card], card))
+    cd = dict_to_difflist(deck1, deck2)
+    output_format = '%-30s|%-5s|%-5s|%-5s'
+    print output_format % ('Card', 'Deck1', 'Both', 'Deck2')
+    for card, (d1, both, d2) in cd.iteritems():
+        #print '%-12i%-12i' % (10 ** i, 20 ** i)
+        print output_format % (card, d1, both, d2)
 
-    for i in difflist1:
-        print 'deck1 :',i
-
-    for i in difflist2:
-        print 'deck2 :',i
 
 if __name__ == '__main__':
     main(sys.argv[1:])
 
     
-def dict_to_difflist(dict1, dict2):
-    return 0
-
-
